8ad06002c330b1b49407a67d3b7802663a18cc32
abhishek baari <bariabhishek14@gmail.com>
2019-05-14 11:18:12 +0530

    jocon
    half_com

---
+++ app/src/main/java/startup/abhishek/spleshscreen/JobConfirmLayout.java
@@ -0,0 +1,14 @@
+package startup.abhishek.spleshscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobConfirmLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_job_confirm_layout );
+    }
+}
