a247266e9a5ed52718c019b8f953c2ce56268f63
abhishek baari <bariabhishek14@gmail.com>
2019-06-14 11:15:00 +0530

    abhishek
    profile

---
+++ app/src/main/java/com/wikav/voulu/Profile_Schoolian_student.java
@@ -0,0 +1,14 @@
+package com.wikav.voulu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile_Schoolian_student extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_profile__schoolian_student );
+    }
+}
